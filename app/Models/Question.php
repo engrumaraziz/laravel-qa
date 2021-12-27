f355a77605cd42caf9d9144720aeba30cd707004
Umar Aziz <umaraziz5057@gmail.com>
2021-12-27 19:50:11 +0500

    changing in models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -10,6 +10,7 @@ class Question extends Model
     use HasFactory;
 
     protected $fillable = ['title','body'];
+    
     public function user(){
         return $this->belongsTo(User::class);
     }
