2c1cffe2d7f07ea37baa6fc17d3625491742fe91
Umar Aziz <umaraziz5057@gmail.com>
2022-01-02 23:49:27 +0500

    add policy athurization

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,16 +37,20 @@
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ms-auto">
-                                        @if (Auth::user()->can('update-question', $question)) {{-- not working --}}
+                                        {{-- @if (Auth::user()->can('update-question', $question)) not working --}}
+                                        @can('update', $question)
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endif
-                                        @if (Auth::user()->can('update-question', $question)) {{-- not working --}}
+                                        @endcan
+                                        {{-- @endif --}}
+                                        {{-- @if (Auth::user()->can('update-question', $question)) not working --}}
+                                        @can('update', $question)
                                         <form class="form-delete" method="POST" action="{{ route('questions.destroy',$question->id) }}">
                                             @method('DELETE')
                                             @csrf
                                             <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure!')">Delete</button>
                                         </form>
-                                        @endif
+                                        @endcan
+                                        {{-- @endif --}}
                                     </div>
                                 </div>
                                 <p class="lead">
