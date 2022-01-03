e0498879935558fc0c3ae43ba510ed3fec18a7d7
Umar Aziz <umaraziz5057@gmail.com>
2022-01-03 18:31:25 +0500

    display answer

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -32,4 +32,8 @@ class Answer extends Model
             // $answer->$question->save();
         });
     }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 }
